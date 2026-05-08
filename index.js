e9ccc2c281d1aa7e7490c81341a06afb68ea341b
Obed Korankye <obedkorankye91@gmail.com>
2026-05-08 22:58:32 +0000

    Feature: Adjusted UI/UX on mobile display

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const navbarToggle = document.querySelector(".navbar-toggle");
 const navbarMenu = document.querySelector(".navbar-menu");
+const navLinks = document.querySelectorAll(".navbar-menu li a");
 
 navbarToggle.addEventListener("click", () => {
   navbarToggle.classList.toggle("active");
   navbarMenu.classList.toggle("active");
+});
+
+// CLOSE MENU WHEN A LINK IS CLICKED
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarToggle.classList.remove("active");
+    navbarMenu.classList.remove("active");
+  });
 });
